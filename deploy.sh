2667b2b7a60fe7c17e30e372908b11eeafff8795
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-14 12:45:16 -0300

    deploy v04141245

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 VERSION="v$(date +%m%d%H%M)"
+VER_SHORT="$(date +%m%d%H%M)"
 echo "🚀 Deploy DMTechApp $VERSION"
 
-# Atualiza versão em todos os arquivos JS/HTML
+# Atualiza versão em todos os arquivos JS
 find . -name "*.js" -not -path "./.git/*" -exec sed -i "s/const VERSION = 'v[0-9]*'/const VERSION = '$VERSION'/g" {} \;
+
+# Cache busting: atualiza ?v= nas tags HTML
+find . -name "*.html" -not -path "./.git/*" -exec sed -i "s/?v=[0-9]*/?v=$VER_SHORT/g" {} \;
 
 git add -A
 git commit -m "deploy $VERSION"
