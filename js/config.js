2f5d12217bdd36259db3d99c9bb24e9620e07575
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-14 16:33:53 -0300

    deploy v04141633

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://lpgpyiwvailshltlkitm.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwZ3B5aXd2YWlsc2hsdGxraXRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNzgwOTMsImV4cCI6MjA5MTc1NDA5M30.p7gsvRbn_JXveOeYFIKQPOAQViwaYJ1LonsEyWHTcA8';
-const VERSION = 'v04141626';
+const VERSION = 'v04141633';
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
