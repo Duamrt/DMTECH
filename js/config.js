ad4959d82516b5962a436e1ecd51b886cdc90c47
Duam Rodrigues <3drxdzin@gmail.com>
2026-05-02 06:42:35 -0300

    deploy v05020642

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://lpgpyiwvailshltlkitm.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_90Ht6_j8rnJKjRA4YWp9Xw_5rTgoOh6';
-const VERSION = 'v05020628';
+const VERSION = 'v05020642';
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v05020628';
+const VERSION = 'v05020642';
 const CACHE = 'dmtech-' + VERSION;
 
 self.addEventListener('install', e => {
